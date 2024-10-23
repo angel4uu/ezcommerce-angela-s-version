53b239058ed49a3168600c5016b8cb89a8e79d3f
AlwinD <edudavilaraffo@gmail.com>
2024-10-22 23:47:48 -0500

    feat: call to action

--- Desarrollo/PVCU/Fuentes/frontend/src/pages/Epica03/MainPage.tsx
+++ Desarrollo/PVCU/Fuentes/frontend/src/pages/Epica03/MainPage.tsx
@@ -1,14 +1,44 @@
 import { Helmet } from 'react-helmet-async';
+import { useNavigate } from 'react-router';
 
 export const MainPage = () => {
+
+  const navigate = useNavigate();
+  const goToLogin = () => navigate('/login')
+
+
   return (
     <>
       <Helmet>
         <title>Ezcommerce</title>
       </Helmet>
       
-      <div className="h-[400px]">
+      <div className="container w-full mx-auto min-h-[400px]">
+        <div>
 
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div className='flex flex-col my-8'>
+          <div>
+            <h3 className='text-center text-xl font-black mb-2 text-secondaryLight dark:text-secondaryDark'>VAMOS</h3>
+            <h2 className='text-center text-4xl font-black mb-2 text-terciaryLight dark:text-terciaryDark '>¿Listo para que todos en la universidad conozca tu marca?</h2>
+            <h3 className='text-center text-xl font-semibold text-terciaryLight dark:text-terciaryDark'>¡Hazla destacar y empieza a vender hoy mismo!</h3>
+          </div>
+          <div className='mt-8 py-4 mx-auto'>
+            <button className='w-[200px] h-[45px] rounded-lg text-lg shadow-xl bg-secondaryLight hover:bg-secondaryLightHovered text-white'
+              onClick={() => goToLogin()}>
+              Iniciar
+            </button>
+          </div>
+        </div>
       </div>
     </>
   )
--- Desarrollo/PVCU/Fuentes/frontend/tailwind.config.js
+++ Desarrollo/PVCU/Fuentes/frontend/tailwind.config.js
@@ -16,6 +16,7 @@ export default {
 				//Modo claro
 				primaryLight:'rgb(255,255,255)',//blanco (fondo)
 				secondaryLight:'rgb(0,54,105)',//azul
+				secondaryLightHovered: '#033c70',//azul clarito
 				terciaryLight:'rgb(54,54,54)',//gris oscuro (fuente)  
 				//Modo oscuro
 				primaryDark:'rgb(31,34,37)',//negro - gris oscuro (fondo)
