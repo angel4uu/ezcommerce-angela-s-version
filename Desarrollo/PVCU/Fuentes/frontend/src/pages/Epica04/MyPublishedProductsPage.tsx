be9655e87039b30a93eee18f9f24726ffb87aee4
Isao25 <shamir.mantilla@unmsm.edu.pe>
2024-10-23 00:58:03 -0500

    add MyPublishedProductsPage

--- Desarrollo/PVCU/Fuentes/frontend/src/pages/Epica04/MyPublishedProductsPage.tsx
+++ Desarrollo/PVCU/Fuentes/frontend/src/pages/Epica04/MyPublishedProductsPage.tsx
@@ -1,4 +1,8 @@
 import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
+
+import { ChevronLeft, CirclePlus,  } from "lucide-react";
+import { Button } from '@/components/ui/button';
 
 export const MyPublishedProductsPage = () => {
   return (
@@ -7,7 +11,19 @@ export const MyPublishedProductsPage = () => {
         <title>Mis productos publicados</title>
       </Helmet>
 
-      <div>MyPublishedProductsPage</div>
+      <div className='my-3 py-2 px-12 space-y-4'>
+        <div className='text-4xl font-sans'>
+          <ChevronLeft className="h-6 w-6 mr-4 inline-block" />
+          <Link to='/products-management'>Productos publicados</Link>
+        </div>
+        <div className='flex items-center justify-between py-2 px-3'>
+          <p className='text-base'>Presiona en uno de los productos para actualizar sus datos</p>
+          <Button className='py-3 px-6 text-base bg-secondaryLight hover:bg-blue-900'>
+            <CirclePlus className='h-4 w-4 inline-block mr-4 text-secondaryLight bg-white rounded-lg' />
+            <Link to='/publish-product'>Publicar producto</Link>
+          </Button>
+        </div>
+      </div>
     </>
   )
 }
