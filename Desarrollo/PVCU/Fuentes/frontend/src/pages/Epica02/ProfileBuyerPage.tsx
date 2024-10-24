c6f29efbec65594137b29d547c5028d3ad725a4b
Vilberto02 <albertojulcapatricio@gmail.com>
2024-10-23 20:33:03 -0500

    feat: Agregar enlace a productos publicados y gestion de compras

--- Desarrollo/PVCU/Fuentes/frontend/src/pages/Epica02/ProfileBuyerPage.tsx
+++ Desarrollo/PVCU/Fuentes/frontend/src/pages/Epica02/ProfileBuyerPage.tsx
@@ -5,7 +5,9 @@ import { Helmet } from "react-helmet-async";
 import { useState, useEffect } from "react";
 import { Switch } from "@/components/ui/switch";
 import { CarruselProducts } from "./CarruselProducts";
-import { ArrowUpRight } from "lucide-react";
+import { ArrowUpRight, Eye, EyeOff } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+import { Link } from "react-router-dom";
 
 interface UserData {
   name: string;
@@ -15,6 +17,10 @@ interface UserData {
   faculty: string;
   email: string;
   password: string;
+  isMarca: boolean;
+  nameMarca: string;
+  logoMarca: string;
+  descriptionMarca: string;
 }
 
 export const ProfileBuyerPage = () => {
@@ -26,9 +32,14 @@ export const ProfileBuyerPage = () => {
     faculty: "",
     email: "",
     password: "",
+    isMarca: false,
+    nameMarca: "",
+    logoMarca: "",
+    descriptionMarca: "",
   });
 
   const [isEditing, setIsEditing] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   useEffect(() => {
     setUserData({
@@ -39,6 +50,11 @@ export const ProfileBuyerPage = () => {
       faculty: "FCB",
       email: "hey@unmsm.com",
       password: "password",
+      isMarca: false,
+      nameMarca: "Electrónica SM",
+      logoMarca: "Mi_Logo.png",
+      descriptionMarca:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in mauris eget sem mollis fringilla eu non magna. Ut bibendum nisl eget congue malesuada.",
     });
   }, []);
 
@@ -51,7 +67,9 @@ export const ProfileBuyerPage = () => {
     setIsEditing(false);
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     setUserData({ ...userData, [e.target.name]: e.target.value });
   };
 
@@ -62,8 +80,51 @@ export const ProfileBuyerPage = () => {
       </Helmet>
 
       <div className="flex flex-col justify-center items-start gap-7 self-stretch my-6">
-        <h3 className="text-2xl font-semibold">Mi Información</h3>
+        <div className="self-stretch">
+          <h3 className="text-2xl font-semibold text-terciaryLight">
+            Mi Información
+          </h3>
+          <p className="mt-4 text-[#555] text-[16px]">
+            Aquí puedes observar tus datos y productos
+          </p>
+        </div>
         <form className="flex flex-col items-start gap-6 self-stretch">
+          {userData.isMarca ? (
+            <div className="flex flex-col items-start gap-6 self-stretch">
+              <Label className="flex flex-col gap-2 self-stretch">
+                Nombre de la Marca
+                <Input
+                  id="nameMarca"
+                  name="nameMarca"
+                  value={userData.nameMarca}
+                  onChange={handleChange}
+                  disabled={!isEditing}
+                ></Input>
+              </Label>
+              <Label className="flex flex-col gap-2 self-stretch">
+                Logo de la Marca
+                <Input
+                  id="logoMarca"
+                  name="logoMarca"
+                  value={userData.logoMarca}
+                  onChange={handleChange}
+                  disabled={!isEditing}
+                ></Input>
+              </Label>
+              <Label className="flex flex-col gap-2 self-stretch">
+                Descripción de la Marca
+                <Textarea
+                  id="descriptionMarca"
+                  name="descriptionMarca"
+                  value={userData.descriptionMarca}
+                  onChange={handleChange}
+                  disabled={!isEditing}
+                ></Textarea>
+              </Label>
+            </div>
+          ) : (
+            ""
+          )}
           <Label className="flex flex-col gap-2 self-stretch">
             Nombres y apellidos
             <Input
@@ -74,7 +135,7 @@ export const ProfileBuyerPage = () => {
               disabled={true}
             ></Input>
           </Label>
-          <div className="flex self-stretch gap-12">
+          <div className="flex flex-col gap-6 self-stretch sm:gap-12 sm:flex-row">
             <Label className="flex flex-col gap-2 w-full">
               Mi foto de perfil
               <Input
@@ -96,7 +157,7 @@ export const ProfileBuyerPage = () => {
               ></Input>
             </Label>
           </div>
-          <div className="flex self-stretch gap-12">
+          <div className="flex flex-col gap-6 self-stretch sm:gap-12 sm:flex-row">
             <Label className="flex flex-col gap-2 w-full">
               Código institucional
               <Input
@@ -131,28 +192,43 @@ export const ProfileBuyerPage = () => {
           </Label>
           <Label className="flex flex-col gap-2 self-stretch">
             Contraseña
-            <Input
-              id="password"
-              name="password"
-              type="password"
-              value={userData.password}
-              onChange={handleChange}
-              disabled={!isEditing}
-            ></Input>
+            <div className="relative">
+              <Input
+                id="password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                value={userData.password}
+                onChange={handleChange}
+                disabled={!isEditing}
+              ></Input>
+              {showPassword ? (
+                <EyeOff
+                  className="absolute top-2 right-4 cursor-pointer"
+                  size={18}
+                  onClick={() => setShowPassword(!showPassword)}
+                ></EyeOff>
+              ) : (
+                <Eye
+                  className="absolute top-2 right-4 cursor-pointer"
+                  size={18}
+                  onClick={() => setShowPassword(!showPassword)}
+                ></Eye>
+              )}
+            </div>
           </Label>
           <div>
             {isEditing ? (
               <div className="flex gap-4">
                 <Button
                   onClick={handleSave}
-                  className="bg-[rgba(0,54,105,1)] hover:bg-[rgba(0,54,105,0.9)]"
+                  className="bg-secondaryLight hover:bg-[rgba(0,54,105,0.9)]"
                 >
                   Guardar datos
                 </Button>
                 <Button
                   variant="outline"
                   onClick={handleSave}
-                  className="border-[rgba(0,54,105,1)] text-[rgba(0,54,105,1)]"
+                  className="border-secondaryLight text-secondaryLight"
                 >
                   Cancelar
                 </Button>
@@ -160,7 +236,7 @@ export const ProfileBuyerPage = () => {
             ) : (
               <Button
                 onClick={handleEdit}
-                className="bg-[rgba(0,54,105,1)] hover:bg-[rgba(0,54,105,0.9)]"
+                className="bg-secondaryLight hover:bg-[rgba(0,54,105,0.9)]"
               >
                 Editar datos
               </Button>
@@ -173,34 +249,48 @@ export const ProfileBuyerPage = () => {
         </form>
       </div>
       <div>
-        <h3 className="text-2xl font-semibold">Notificaciones</h3>
-        <Label className="my-6 flex py-1 px-4 justify-between text-lg font-medium">
+        <h3 className="text-2xl font-semibold text-terciaryLight">
+          Notificaciones
+        </h3>
+        <Label className="my-6 flex py-1 px-4 justify-between items-center text-lg font-medium">
           Permitir notificaciones
-          <Switch className="data-[state=checked]:bg-[rgba(0,54,105,1)]" />
+          <Switch className="data-[state=checked]:bg-secondaryLight" />
         </Label>
       </div>
       <div className="flex flex-col w-full gap-6 self-stretch my-6">
         <div className="flex justify-between items-center">
-          <h3 className="text-2xl font-semibold">Productos comprados</h3>
-          <div className="bg-[rgba(183,183,183,0.30)] p-2 rounded-full cursor-pointer">
-            <ArrowUpRight className="text-2xl" />
-          </div>
+          <h3 className="text-2xl font-semibold text-terciaryLight">
+            Productos comprados
+          </h3>
+          <Link
+            to="/purchasing-management"
+            className="bg-[rgba(183,183,183,0.30)] p-2 rounded-full"
+          >
+            <ArrowUpRight className="text-2xl text-terciaryLight" />
+          </Link>
         </div>
         <CarruselProducts></CarruselProducts>
       </div>
       <div className="flex flex-col w-full gap-6 self-stretch my-6">
         <div className="flex justify-between items-center">
-          <h3 className="text-2xl font-semibold">Productos vendidos</h3>
-          <div className="bg-[rgba(183,183,183,0.30)] p-2 rounded-full cursor-pointer">
-            <ArrowUpRight className="text-2xl" />
-          </div>
+          <h3 className="text-2xl font-semibold text-terciaryLight">
+            Productos vendidos
+          </h3>
+          <Link
+            to=""
+            className="bg-[rgba(183,183,183,0.30)] p-2 rounded-full cursor-pointer"
+          >
+            <ArrowUpRight className="text-2xl text-terciaryLight" />
+          </Link>
         </div>
         <CarruselProducts></CarruselProducts>
       </div>
       <div className="mb-24">
-        <h3 className="text-2xl font-semibold">Eliminar cuenta</h3>
-        <div className="flex justify-between items-center">
-          <Label className="my-6 flex py-1 px-4 justify-between text-lg font-medium lg:items-center sm:items-start">
+        <h3 className="text-2xl font-semibold mb-6 text-terciaryLight">
+          Eliminar cuenta
+        </h3>
+        <div className="flex justify-between items-start sm:items-center">
+          <Label className="flex py-1 px-4 justify-between text-lg font-medium lg:items-center sm:items-start">
             Una vez que elimines tu cuenta, no podrás volver atrás. Asegúrate de
             hacerlo.
           </Label>
