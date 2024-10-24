7fbae4b0f0cd2805e08d7cd1e900d9ea9d60d358
miguelespp <carlos.espinoza24@unmsm.edu.pe>
2024-10-23 23:18:21 -0500

    feat: finish epica1

--- Desarrollo/PVCU/Fuentes/backend/epica1/urls.py
+++ Desarrollo/PVCU/Fuentes/backend/epica1/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
-from .views import RegisterView
+from django.urls import path
+from .views import RegisterView, UserProfielView
 
 urlpatterns = [
-    path('register', RegisterView.as_view(), name='register')
+    path('register', RegisterView.as_view(), name='register'),
+    path('profile/', UserProfielView.as_view(), name='profile'),
 ]
--- Desarrollo/PVCU/Fuentes/backend/epica1/views.py
+++ Desarrollo/PVCU/Fuentes/backend/epica1/views.py
@@ -1,12 +1,14 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 from .serializers import UserSerializer
 
 # Create your views here.
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -14,3 +16,18 @@ class RegisterView(generics.CreateAPIView):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+class UserProfielView(APIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        user_data = {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name
+        }
+        return Response(user_data)
