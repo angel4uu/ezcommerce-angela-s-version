89511fbc410bd6d96d9d2ef12dabcd7e64342b78
angelajarae <angelajaraespinoza@gmail.com>
2024-10-28 10:54:04 -0500

    User authentication functionality

--- Desarrollo/PVCU/Fuentes/backend/ezcommerce/settings.py
+++ Desarrollo/PVCU/Fuentes/backend/ezcommerce/settings.py
@@ -43,10 +43,12 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'epica1',
     'epica2',
-    'epica4'
+    'epica4',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +56,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Frontend server
 ]
 
 REST_FRAMEWORK = {
--- Desarrollo/PVCU/Fuentes/frontend/src/EzCommercerApp.tsx
+++ Desarrollo/PVCU/Fuentes/frontend/src/EzCommercerApp.tsx
@@ -1,11 +1,14 @@
 import routes from "./router/AppRouter"
 import { RouterProvider } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import {AuthProvider} from "./context/AuthProvider";
 
 export const EzCommercerApp = () => {
   return (
     <HelmetProvider>
+      <AuthProvider>
         <RouterProvider router={routes} />
+      </AuthProvider>
     </HelmetProvider>
   )
 }
