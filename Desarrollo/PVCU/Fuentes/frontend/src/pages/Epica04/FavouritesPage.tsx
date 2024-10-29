3439b39869f2a0b67d51136943787438d9fb2dc7
Isao25 <shamir.mantilla@unmsm.edu.pe>
2024-10-29 01:53:08 -0500

    refactor: remove Link

--- Desarrollo/PVCU/Fuentes/frontend/src/pages/Epica04/FavouritesPage.tsx
+++ Desarrollo/PVCU/Fuentes/frontend/src/pages/Epica04/FavouritesPage.tsx
@@ -1,15 +1,10 @@
 import { Helmet } from 'react-helmet-async';
-import { Link, useNavigate } from 'react-router-dom';
 
 import { CardFavourite } from "../../components/Epica04/CardFavourite";
-import { ChevronLeft } from 'lucide-react';
+
 
 export const FavouritesPage = () => {
-  const navigate = useNavigate();
 
-  const handleGoBack = () => {
-    navigate(-1);
-  };
   return (
     <>
       <Helmet>
@@ -18,10 +13,9 @@ export const FavouritesPage = () => {
 
       <div className='my-3 py-2 px-12 space-y-4'>
         <div className='text-4xl font-sans items-center'>
-            <ChevronLeft className="h-6 w-6 mr-4 inline-block" />
-            <Link to='#' onClick={handleGoBack}>Productos que te gustan</Link>
+          <span>Productos que te gustan</span>
         </div>
-        <div className='flex items-center py-2 px-3'>
+        <div className='flex items-center py-2 px-0'>
             <p className='text-base'>Aquí puedes encontrar todos tus productos favoritos.</p>
           
         </div>
