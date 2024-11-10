146e4d468e87b049b429eb6427cad4611c28534e
miguelespp <carlos.espinoza24@unmsm.edu.pe>
2024-11-09 21:47:42 -0500

    feat: add autodocumentation with swagger

--- Desarrollo/PVCU/Fuentes/backend/ezcommerce/settings.py
+++ Desarrollo/PVCU/Fuentes/backend/ezcommerce/settings.py
@@ -45,8 +45,22 @@ INSTALLED_APPS = [
     'epica2',
     'epica4',
     'epica5',
-    'corsheaders'
+    'epica8',
+    'corsheaders',
+    'drf_yasg',
 ]
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    },
+    'USE_SESSION_AUTH': False,
+}
+
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
--- Desarrollo/PVCU/Fuentes/backend/ezcommerce/urls.py
+++ Desarrollo/PVCU/Fuentes/backend/ezcommerce/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
+from .swagger import schema_view
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/', include([
         path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
         path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
