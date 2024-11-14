4c00bb0d0fd4c7284a981594b58232bc56f07017
GiacomoMadrid <gsmr.18@gmail.com>
2024-11-14 08:17:58 -0500

    Foreign Key: value

--- Desarrollo/PVCU/Fuentes/backend/epica4/serializers.py
+++ Desarrollo/PVCU/Fuentes/backend/epica4/serializers.py
@@ -1,22 +1,26 @@
 from rest_framework import serializers
 from .models import Etiqueta, Catalogo, Articulo, Imagen
 
-class EtiquetaSerializer(serializers.HyperlinkedModelSerializer):
+class EtiquetaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Etiqueta
         fields = '__all__'
 
-class CatalogoSerializer(serializers.HyperlinkedModelSerializer):
+class CatalogoSerializer(serializers.ModelSerializer):
+    id_usuario = serializers.IntegerField(source='id_usuario.id')
+    id_marca = serializers.IntegerField(source='id_marca.id')
     class Meta:
         model = Catalogo
         fields = '__all__'
 
-class ArticuloSerializer(serializers.HyperlinkedModelSerializer):
+class ArticuloSerializer(serializers.ModelSerializer):    
+    id_catalogo = serializers.IntegerField(source='id_catalogo.id')
     class Meta:
         model = Articulo
         fields = '__all__'
 
-class ImagenSerializer(serializers.HyperlinkedModelSerializer):
+class ImagenSerializer(serializers.ModelSerializer):    
+    id_articulo = serializers.IntegerField(source='id_articulo.id')
     class Meta:
         model = Imagen
         fields = '__all__'
--- Desarrollo/PVCU/Fuentes/backend/epica8/serializers.py
+++ Desarrollo/PVCU/Fuentes/backend/epica8/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import *
 
 class ReporteSerializer(serializers.ModelSerializer):
+    id_usuario = serializers.IntegerField(source='id_usuario.id')
     class Meta:
         model = Reporte
         fields = '__all__'
