78a5d14020263fdc58fc59c7271cf92c9d0aafe5
GiacomoMadrid <gsmr.18@gmail.com>
2024-11-14 07:50:53 -0500

    Epica3 finalizada

--- Desarrollo/PVCU/Fuentes/backend/epica2/serializers.py
+++ Desarrollo/PVCU/Fuentes/backend/epica2/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from .models import *
 
-class FacultadSerializer(serializers.HyperlinkedModelSerializer):
+class FacultadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Facultad
         fields = '__all__'
 
-class EscuelaProfesionalSerializer(serializers.HyperlinkedModelSerializer):
+class EscuelaProfesionalSerializer(serializers.ModelSerializer):
+    id_facultad = serializers.IntegerField(source='id_facultad.id')
     class Meta:
         model = EscuelaProfesional
         fields = '__all__'
