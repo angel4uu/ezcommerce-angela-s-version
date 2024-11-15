90f197b09b12f49bcd15e5fa0c1a125db301fe45
AlwinD <edudavilaraffo@gmail.com>
2024-11-15 04:23:32 -0500

    fix margin

--- Desarrollo/PVCU/Fuentes/frontend/src/pages/Epica03/SearchPage.tsx
+++ Desarrollo/PVCU/Fuentes/frontend/src/pages/Epica03/SearchPage.tsx
@@ -236,7 +236,7 @@ export const SearchPage = () => {
               />
             </div>
           </div>
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 p-4'>
             {items ? items.map((p) => (
               <ProductCard key={p.id} id={p.id} name={p.nombre} price={p.precio} qualification={4} img={''} />
             )) : null}
