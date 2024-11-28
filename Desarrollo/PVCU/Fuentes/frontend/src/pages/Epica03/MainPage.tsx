042eab5772ac5b4827fd2760df97de6d90d33828
Isao25 <shamir.mantilla@unmsm.edu.pe>
2024-11-28 05:25:38 -0500

    Add imageUrl to Articulo interface and integrate image fetching in SearchPage

--- Desarrollo/PVCU/Fuentes/frontend/src/pages/Epica03/MainPage.tsx
+++ Desarrollo/PVCU/Fuentes/frontend/src/pages/Epica03/MainPage.tsx
@@ -17,6 +17,7 @@ import { useEffect, useState } from 'react';
 import { Articulo, getArticulos } from '../../api/apiArticulos';
 import { EscuelaProfesional, Usuario } from '../../types';
 import { escuelasService, usuariosApi } from '../../api/apiUsuarios';
+import { getAllImages } from '@/api/apiImages';
 
 
 export const MainPage = () => {
@@ -42,21 +43,46 @@ export const MainPage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const articulos = await getArticulos()
-        setProducts(articulos.data.results)
 
-        const escuelas = await escuelasService.getEscuelas()
-        setEscuelas(escuelas.data.results)
+        const articulosResponse = await getArticulos();
+        const articulos = articulosResponse.data.results;
+  
+        const imagesResponse = await getAllImages();
+        const images = imagesResponse.data.results;
+  
+        interface Image {
+          id_articulo: number;
+          url: string;
+        }
 
-        const vendedores = await usuariosApi.get('?es_vendedor=true')
-        setSellers(vendedores.data.results)
+        const imageMap = images.reduce((acc: Record<number, string>, img: Image) => {
+          if (!acc[img.id_articulo]) {
+            acc[img.id_articulo] = img.url; 
+          }
+          return acc;
+        }, {} as Record<number, string>);
+  
+        const productsWithImages = articulos.map((product:Articulo) => ({
+          ...product,
+          imageUrl: imageMap[product.id] || null,
+        }));
+  
+        setProducts(productsWithImages);
 
+        const escuelasResponse = await escuelasService.getEscuelas();
+        setEscuelas(escuelasResponse.data.results);
+  
+        const vendedoresResponse = await usuariosApi.get('?es_vendedor=true');
+        setSellers(vendedoresResponse.data.results);
       } catch (error) {
-        throw error
+        console.error("Error al cargar los datos:", error);
       }
-    }
-    fetchData()
-  }, [])
+    };
+  
+    fetchData();
+  }, []);
+  
+  
 
   return (
     <>
@@ -65,58 +91,71 @@ export const MainPage = () => {
       </Helmet>
 
       <div className="container w-full mx-auto">
-        <div className='my-8'>
-          <Carousel opts={{
-            loop: true
-          }}>
+        <div className="my-8">
+          <Carousel
+            opts={{
+              loop: true,
+            }}
+          >
             <CarouselContent>
-              {
-                images.map((i, index) => (
-                  <CarouselItem>
-                    <div key={`d-${index}`} className="overflow-hidden rounded-lg shadow-lg max-h-[500px]">
-                      <img
-                        key={`i-${index}`}
-                        src={i.src}
-                        alt={i.alt}
-                        className="w-full h-full object-cover"
-                      />
-                    </div>
-                  </CarouselItem>
-                ))
-              }
+              {images.map((i, index) => (
+                <CarouselItem>
+                  <div
+                    key={`d-${index}`}
+                    className="overflow-hidden rounded-lg shadow-lg max-h-[500px]"
+                  >
+                    <img
+                      key={`i-${index}`}
+                      src={i.src}
+                      alt={i.alt}
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                </CarouselItem>
+              ))}
             </CarouselContent>
           </Carousel>
         </div>
-        <div className=' justify-center mx-auto max-w-[1350px] px-8 '>
-          <Carousel opts={{
-            align: 'start',
-            loop: true,
-            dragFree: true
-          }} >
-            <CarouselContent className='mb-12 mt-4 mx-auto px-20'>
-              {
-                products.map((p) => (
-                  <>
-                    <CarouselItem key={`ci-${p.id}`} className='basis-1/1 lg:basis-1/2 xl:basis-1/3 2xl:basis-1/4'>
-                      <ProductCard key={p.id} id={p.id} name={p.nombre} price={p.precio} qualification={4.4} img={''} />
-                    </CarouselItem>
-                  </>
-                ))
-              }
+        <div className=" justify-center mx-auto max-w-[1350px] px-8 ">
+          <Carousel
+            opts={{
+              align: "start",
+              loop: false,
+              dragFree: true,
+            }}
+          >
+            <CarouselContent className="mb-12 mt-4 mx-auto px-20">
+              {products.map((p) => (
+                <CarouselItem
+                  key={`ci-${p.id}`}
+                  className="basis-1/1 lg:basis-1/2 xl:basis-1/3 2xl:basis-1/4"
+                >
+                  <ProductCard
+                    key={p.id}
+                    id={p.id}
+                    name={p.nombre}
+                    price={p.precio}
+                    qualification={4.4}
+                    img={p.imageUrl || "default-image-url.jpg"}
+                  />
+                </CarouselItem>
+              ))}
             </CarouselContent>
-            <div className='hidden md:block '>
+
+            <div className="hidden md:block ">
               <CarouselPrevious className="bg-white w-10 h-10 border-0 shadow-md shadow-[#767676]" />
               <CarouselNext className="bg-white w-10 h-10 border-0 shadow-md shadow-[#767676]" />
             </div>
-
           </Carousel>
         </div>
 
-        <div className='mx-auto w-full p-4'>
-          <div className=' mb-5 '>
-            <h2 className='text-left text-4xl font-bold mb-2 text-terciaryLight dark:text-terciaryDark '>Elige tu categoría favorita</h2>
+        <div className="mx-auto w-full p-4">
+          <div className=" mb-5 ">
+            <h2 className="text-left text-4xl font-bold mb-2 text-terciaryLight dark:text-terciaryDark ">
+              Elige tu categoría favorita
+            </h2>
           </div>
-          <div className='grid md:grid-cols-2 lg:grid-cols-4 grid-rows-4 md:grid-rows-2 gap-4 px-4 xl:px-16'>
+          <div className="grid md:grid-cols-2 lg:grid-cols-4 grid-rows-4 md:grid-rows-2 gap-4 px-4 xl:px-16">
             {categories.map((category, index) => (
               <CategoriesCard
                 id={category.id.toString()}
@@ -128,50 +167,61 @@ export const MainPage = () => {
                 index={index}
               />
             ))}
-
           </div>
         </div>
-        <div className='mx-auto mb-10'>
-          <div className='mt-8 mb-5'>
-            <h2 className='text-left text-4xl font-bold mb-2 text-terciaryLight dark:text-terciaryDark '>Conoce a nuestro vendedores destacados</h2>
+        <div className="mx-auto mb-10">
+          <div className="mt-8 mb-5">
+            <h2 className="text-left text-4xl font-bold mb-2 text-terciaryLight dark:text-terciaryDark ">
+              Conoce a nuestro vendedores destacados
+            </h2>
           </div>
-          <div className='mt-3 relative'>
-            <Carousel opts={{
-              loop: true,
-              dragFree: true
-            }} >
-              <CarouselContent className='mb-24 mx-2'>
-                {
-                  sellers.map((seller, index) => (
-                    <>
-                      <CarouselItem key={`ci-${index}`} className='basis-1/1 sm:basis-1/2 md:basis-1/3 xl:basis-1/5'>
-                        <SellersCard
-                          key={`sc-${index}`}
-                          id={seller.id?.toString()||index.toString()}
-                          name={`${seller.nombres} ${seller.apellido_p}`}
-                        />
-                      </CarouselItem>
-                    </>
-                  ))
-                }
+          <div className="mt-3 relative">
+            <Carousel
+              opts={{
+                loop: true,
+                dragFree: true,
+              }}
+            >
+              <CarouselContent className="mb-24 mx-2">
+                {sellers.map((seller, index) => (
+                  <>
+                    <CarouselItem
+                      key={`ci-${index}`}
+                      className="basis-1/1 sm:basis-1/2 md:basis-1/3 xl:basis-1/5"
+                    >
+                      <SellersCard
+                        key={`sc-${index}`}
+                        id={seller.id?.toString() || index.toString()}
+                        name={`${seller.nombres} ${seller.apellido_p}`}
+                      />
+                    </CarouselItem>
+                  </>
+                ))}
               </CarouselContent>
               <div className="absolute bottom-8  left-1/2 transform -translate-x-1/3 flex space-x-10 ">
                 <CarouselPrevious className="bg-[#B7B7B7]" />
                 <CarouselNext className="bg-[#B7B7B7]  " />
               </div>
             </Carousel>
-
           </div>
         </div>
-        <div className='flex flex-col my-8'>
+        <div className="flex flex-col my-8">
           <div>
-            <h3 className='text-center text-xl font-black mb-2 text-secondaryLight dark:text-secondaryDark'>VAMOS</h3>
-            <h2 className='text-center text-4xl font-black mb-2 text-terciaryLight dark:text-terciaryDark '>¿Listo para que todos en la universidad conozca tu marca?</h2>
-            <h3 className='text-center text-xl font-semibold text-terciaryLight dark:text-terciaryDark'>¡Hazla destacar y empieza a vender hoy mismo!</h3>
+            <h3 className="text-center text-xl font-black mb-2 text-secondaryLight dark:text-secondaryDark">
+              VAMOS
+            </h3>
+            <h2 className="text-center text-4xl font-black mb-2 text-terciaryLight dark:text-terciaryDark ">
+              ¿Listo para que todos en la universidad conozca tu marca?
+            </h2>
+            <h3 className="text-center text-xl font-semibold text-terciaryLight dark:text-terciaryDark">
+              ¡Hazla destacar y empieza a vender hoy mismo!
+            </h3>
           </div>
-          <div className='mt-8 py-4 mx-auto'>
-            <button className='w-[200px] h-[45px] rounded-lg text-lg shadow-xl bg-secondaryLight hover:bg-secondaryLightHovered text-white'
-              onClick={handleTrademarkClick}>
+          <div className="mt-8 py-4 mx-auto">
+            <button
+              className="w-[200px] h-[45px] rounded-lg text-lg shadow-xl bg-secondaryLight hover:bg-secondaryLightHovered text-white"
+              onClick={handleTrademarkClick}
+            >
               Iniciar
             </button>
           </div>
@@ -179,5 +229,5 @@ export const MainPage = () => {
       </div>
       <LoginModal />
     </>
-  )
+  );
 }
